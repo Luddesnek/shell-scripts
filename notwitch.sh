7878fad83f6842a02cac8eadb3982e3eb7ee6b5f
luddesnus <Luddesnek@protonmail.com>
2022-04-30 17:38:50 +0200

    second commit

---
+++ notwitch.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+read -p "[Whom'st content do'ya wanna to consom]$ " STREAMER
+MPV=$(mpv "https://www.twitch.tv/$STREAMER")
+MESSAGE=$(echo "$MPV" | awk '/offline/ {print $5,$6,$7}')
+if echo "$MESSAGE"; then 
+	echo "no consom for you"
+	exit 0
+fi
