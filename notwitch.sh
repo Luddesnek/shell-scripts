2c7a240778c7bae93a74187ec76c77e60b50a46a
Lu <Luddesnek@protonmail.com>
2022-05-04 18:56:07 +0200

    Add files via upload

--- notwitch.sh
+++ notwitch.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 
-read -p "[Whom'st do you want to consom]$ " STREAMER
+read -p "Whom'st do you want to consom : " STREAMER
 MPV=$(mpv "https://www.twitch.tv/$STREAMER")
-MESSAGE=$(echo "$MPV" | awk '/offline/ {print $5,$6,$7}')
+MESSAGE=$(echo "$MPV" | grep 'No summary')
 if echo "$MESSAGE"; then 
 	echo "no consom for you"
 	exit 0
